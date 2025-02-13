cc9c64977f7e3c1abc49b83a0dc1ee6389df7e83
Leo <leonardonifinluri@gmail.com>
2025-02-13 17:20:34 +0800

    loop: do while loop

--- loop/while-loop.php
+++ loop/while-loop.php
@@ -1,9 +1,11 @@
 <?php
 
 /* <============================ WHILE LOOP ============================> */
-//while loop keep execute the same block untill a condition is reached or the conditions is false
-
-//Example: find binary of an integer
+/*
+While loop keep execute the same block untill a condition is reached or the conditions is false
+Check first then execute
+Example: find binary of an integer
+*/
 $res = '';
 $number = 10;
 while($number > 0){
@@ -11,7 +13,7 @@ while($number > 0){
     $res = $q . $res;
     $number = (int)($number/2);
 }
-echo $res;
+echo "While loop:\n".$res."\n\n";
 
 /*
 Explanation :
@@ -20,3 +22,20 @@ Condition : $number > 0
 Update : $number = $number / 2
 */
 /* <============================ END WHILE LOOP ============================> */
+
+
+/* <============================ DO WHILE LOOP ============================> */
+/*
+Do while loop execute the block first then check for the condition
+Example: find binary of a number
+*/
+$res = '';
+$number = 10;
+do{
+    $q = $number%2;
+    $res = $q . $res;
+    $number = (int)($number/2);
+}while($number > 0);
+echo "Do while loop:\n".$res."\n\n";
+/* <============================ END DO WHILE LOOP ============================> */
+
