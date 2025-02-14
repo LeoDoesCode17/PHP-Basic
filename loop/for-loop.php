f1c1e13379b69bbc60ab336481a7013933df5075
Leo <leonardonifinluri@gmail.com>
2025-02-14 12:08:48 +0800

    loop: foreach

--- loop/for-loop.php
+++ loop/for-loop.php
@@ -1,5 +1,25 @@
 <?php
 
+/* <============================ HELPER ============================> */
+class Car{
+    public $model;
+    public $color;
+
+    public function __construct($model, $color) {
+        $this->model = $model;
+        $this->color = $color;
+    }
+}
+
+function newLine(){
+    echo "\n";
+}
+
+function display($message){
+    echo $message;
+}
+/* <============================ END HELPER ============================> */
+
 /* <============================ FOR LOOP ============================> */
 /*
 For loop execute block of code in a certain times
@@ -14,23 +34,67 @@ Explanation :
 */
 
 //Example: print odd number in range 1 - 100
-echo "Odd numbers in range 1-100:\n";
+display("Odd numbers in range 1-100:\n");
 for($i = 1; $i <= 100; $i++){
     if($i%2 === 0){
-        echo $i." ";
+        display($i." ");
     }
 }
-echo "\n";
+newLine();
 
 //We can break the for loop if a condition is satisfied by using break keyword
 //Example: perform linear searching
 $target = 10;
 $arr = [1, 2, 5, 6, 1, -1, 10, 2, 3, 4, -1];
-echo "Using break keyword to perform linear search:\n";
+display("Using break keyword to perform linear search:\n");
 for($i = 0; $i < count($arr); $i++){
     if($arr[$i] === $target){
-        echo "$target found at index $i\n";
+        display("$target found at index $i\n");
         break;
     }
 }
+newLine();
 /* <============================ END FOR LOOP ============================> */
+
+/* <============================ FOREACH ============================> */
+//foreach iterate through for each element in an array
+$arr = [1, 2, 3, 4, 5, 6, 6];
+display("Iterate array using foreach:\n");
+foreach($arr as $number){
+    display("$number\n");
+}
+newLine();
+
+//foreach using key-value (for associative array and object)
+$arr = [
+    'name' => 'Leonardo',
+    'id' => 'D121221020'
+];
+display("Iterate associative array using foreach:\n");
+foreach($arr as $key => $val){
+    display("$key: $val\n");
+}
+newLine();
+
+$car = new Car('Volvo', 'Red');
+display("Iterate car object using foreach:\n");
+foreach($car as $prop => $val){
+    display("$prop: $val\n");
+}
+newLine();
+
+/*
+When change an item using foreach loop, it doesn't effect the original array
+We can modifiy item in array using foreach loop by adding & symbol in the item declaration in foreach loop
+This method is called foreach byref, the array item are asigned by reference
+*/
+$arr = [1, 2, 3, 4, 5, 6];
+foreach($arr as &$number){
+    if($number%2 == 1){
+        $number -= 1;
+    }
+}
+//now $arr = [0, 2, 2, 4, 4, 6]
+var_dump($arr);
+/* <============================ END FOREACH ============================> */
+
